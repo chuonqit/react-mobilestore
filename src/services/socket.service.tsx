c5b1b2155012ba27fcaf1935f63059ef0774936e
ChuongQIT <chuonqit@gmail.com>
2022-04-09 19:49:26 +0700

    io cors

--- src/services/socket.service.tsx
+++ src/services/socket.service.tsx
@@ -1,5 +1,5 @@
 import SocketIOClient from "socket.io-client";
 
-const socketConnect = SocketIOClient("https://nodejs-mobilestore.vercel.app", { secure: true }).connect();
+const socketConnect = SocketIOClient("https://nodejs-mobilestore.vercel.app").connect();
 
 export default socketConnect;
