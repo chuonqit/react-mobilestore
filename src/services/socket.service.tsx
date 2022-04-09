e3fb43cd918e3ab8dcdba4a6425166965e5dd1ca
ChuongQIT <chuonqit@gmail.com>
2022-04-09 15:57:09 +0700

    feat: change socket url

--- src/services/socket.service.tsx
+++ src/services/socket.service.tsx
@@ -1,5 +1,5 @@
 import SocketIOClient from "socket.io-client";
 
-const socketConnect = SocketIOClient("http://localhost:5000").connect();
+const socketConnect = SocketIOClient("https://chuong-mobilestore.vercel.app").connect();
 
 export default socketConnect;
