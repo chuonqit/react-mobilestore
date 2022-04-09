a53e8a5ef4aee99b0cc54be8fcb0957475fdc608
ChuongQIT <chuonqit@gmail.com>
2022-04-09 20:01:38 +0700

    io url server transports websocket

--- src/services/socket.service.tsx
+++ src/services/socket.service.tsx
@@ -1,5 +1,5 @@
 import SocketIOClient from "socket.io-client";
 
-const socketConnect = SocketIOClient("https://nodejs-mobilestore.vercel.app").connect();
+const socketConnect = SocketIOClient("https://nodejs-mobilestore.vercel.app", { transports: ["websocket"] }).connect();
 
 export default socketConnect;
